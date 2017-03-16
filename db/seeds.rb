b016cd8ead8a45812ea810171fa831a687102b28
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-03-16 16:49:25 -0500

    Seeded my database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create!(
+    firstname: Faker::RockBand.name,
+    lastname: Faker::Pokemon.name,
+    username: Faker::Internet.user_name,
+    email:  Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+users = User.all
+
+30.times do
+  Item.create!(
+    user: users.sample,
+    name: Faker::ChuckNorris.fact
+  )
+end
+
+puts 'Seed finished'
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
