8ba52293f2971b72443e2eb34b1ff23e440f3497
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-03-17 16:21:48 -0500

    User story 8 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
-    @user = current_user
+    @user = if params[:id].nil?
+              current_user
+            else
+              User.find(params[:id])
+                                end
     @items = @user.items
   end
 end
